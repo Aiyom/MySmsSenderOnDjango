b632041c1a0ca467ba0bdbf66cc446ed1ca11c33
Aiyom <43129950+Aiyom@users.noreply.github.com>
2021-02-28 19:02:36 +0500

    Update settings.py

--- newVersionSms/settings.py
+++ newVersionSms/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nh!sk9ct3^361y+h340&(^-#ey6o-5lk6@9y*e#k9fq*4wg&*1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
